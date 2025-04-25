22976e06e51a3d17387ad792766908d9f0296fda
Dipika Gandash <dipikadevv1445@gmail.com>
2025-04-25 21:04:47 +0530

    add search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,6 +6,7 @@ const Body = () => {
 
   const [restaurantList, setRestaurantList] = useState([]);
   const [filteredList, setFilteredList] = useState([]);
+  const [searchText, setSearchText] = useState("");
   
 
   useEffect(() => {
@@ -17,11 +18,12 @@ const Body = () => {
     const json = await data.json();
     const restaurants = json?.data.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
     if(restaurants){
+      setFilteredList(restaurants)
       setRestaurantList(restaurants);
     }
   }
 
-
+console.log("Body rendered  again")
  
   return (
     <div className="body-container">
@@ -29,8 +31,8 @@ const Body = () => {
       <div className="filter-search-container">
         <div className="filter-container">
           <button className="filter-btn" onClick={() => {
-            const filteredList = restaurantList.filter((res) => res.info.avgRating > 4.5)
-            setRestaurantList(filteredList)
+            const filteredList = restaurantList.filter((res) => res.info.avgRating > 4.5);
+            setFilteredList(filteredList)
           }}>Top Rated Restaurants</button>
         </div>
 
@@ -39,7 +41,15 @@ const Body = () => {
             type="text"
             className="search-bar"
             placeholder="Search for dishes or cuisines..."
+          value = {searchText}
+          onChange={(e) => setSearchText(e.target.value)}
           />
+          <button className="search-btn" onClick={
+            () => {
+             const filteredList = restaurantList.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+             setFilteredList(filteredList)
+            }
+          }>Search</button>
         </div>
       </div>
 
@@ -47,7 +57,7 @@ const Body = () => {
         {
          restaurantList.length === 0
          ? Array(20).fill(0).map((_, index) => <Shimmer key={index} />)
-         : restaurantList.map((restaurant) => (
+         : filteredList.map((restaurant) => (
              <RestaurantCard 
                key={restaurant.info.id} 
                resData={restaurant.info} 
